def11cbb20072004c464e1d14753507cf5356fc6
cansinicim <cansinicim148@gmail.com>
2025-12-15 18:30:55 +0300

    yaptim

--- backend/src/models/Account.js
+++ backend/src/models/Account.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const accountSchema = new mongoose.Schema({
-  // Define account schema here
+    tc_kimlik: { type: String, required: true, unique: true, match: /^[1-9][0-9]{10}$/ },
+  telefon: { type: String, match: /^0[0-9]{10}$/, required: true },
+  sms_onay: { type: Boolean, default: false },
+  hesap_turu: {type: [String],enum: [ 'Vadesiz Hesap' , 'Vadeli Hesap' , 'Yatırım / Döviz Hesabı' , 'Kredi Kartı' ],default: [], required: true },
+  aylik_gelir: {type: String,enum: ['0 - 10.000 TL','10.000 - 25.000 TL','25.000 - 50.000 TL','50.000 - 100.000 TL','100.000 TL üzeri'],required: true},
+  mal_varlik: {type: [String],enum: [ 'Maaş', 'Kira' , 'Araç' , 'Miras' ],default: [], required: true },
+  islem_hacmi: {type: String,enum: ['0 - 50.000 TL','50.000 - 100.000 TL','100.000 - 500.000 TL','500.000 TL üzeri'],required: true},
+  egitim_durumu: { type: String,enum: ['İlkokul','Ortaokul','Lise','Ön Lisans','Lisans','Yüksek Lisans','Doktora'],required: true},
+  calisma_durumu: { type: String,enum: ['Çalışıyor','Çalışmıyor','Emekli','Öğrenci'],required: true},
+  calisma_sektoru: { type: String,enum: ['Kamu','Özel Sektör','Serbest Meslek','Ticaret','Diğer'],required: true}
 }, {
-  timestamps: true
+timestamps: true
 });
 
 module.exports = mongoose.model('Account', accountSchema);
--- backend/src/models/Transaction.js
+++ backend/src/models/Transaction.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
-  // Define transaction schema here
-}, {
+dekont_url: { type: String, required: true, unique: true },
+alici_ıban: { type: String, required: true, match: /^TR[0-9]{24}$/ },
+gonderici_ıban: { type: String, required: true, match: /^TR[0-9]{24}$/ },
+gonderici_ad_soyad: { type: String, required: true },
+alici_ad_soyad: { type: String, required: true },
+miktar: { type: Number, required: true, min: 0.01 },
+islem_tarihi: { type: Date, default: Date.now},
+kesinti_tutari: { type: Number, required: true, min: 0.00 },
+},
+{
   timestamps: true
 });
 
