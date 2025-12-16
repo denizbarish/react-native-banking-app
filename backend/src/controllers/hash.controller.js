43249c266eb096b393a036e2bf63f363b830436e
denizbarish <denizbarisyi@gmail.com>
2025-12-16 12:04:47 +0300

    hash_bugfix

--- backend/src/controllers/hash.controller.js
+++ backend/src/controllers/hash.controller.js
@@ -1,15 +1,18 @@
 const bcrypt = require('bcryptjs');
+
 const hash = async (req, res) => {
       const { data } = req.body;
       const salt = await bcrypt.genSalt(10);
       const hashdata = await bcrypt.hash(data, salt);
       res.status(200).json({ hashdata });
-  }; 
-  const checkhash = async (req, res) => {
+}; 
+
+const checkhash = async (req, res) => {
         const { data, value } = req.body;
-        const kontrolislemi = await bcrypt.compare(value, data);
+        const kontrolislemi = await bcrypt.compare(this.hash(value), this.hash(data));
         res.status(200).json({ kontrolislemi });  
-  };
+};
+
   module.exports = {
     hash,
     checkhash
