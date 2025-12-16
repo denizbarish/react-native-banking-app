13c75af793ae63339015c8e462f2314619ed27ee
EmirhanErc32 <your@email.com>
2025-12-16 22:13:51 +0300

    hash_bugfix_v3

--- backend/src/controllers/hash.controller.js
+++ backend/src/controllers/hash.controller.js
@@ -1,17 +1,25 @@
 const bcrypt = require('bcryptjs');
 
 const hash = async (req, res) => {
+  try {
       const { data } = req.body;
       const salt = await bcrypt.genSalt(10);
       const hashdata = await bcrypt.hash(data, salt);
-      res.status(200).json({ hashdata });
+      res.status(200).json({ hashdata }); }
+    catch (error) {
+      res.status(500).json({ message: 'Data Hashlemede Hata!' });
+    }
 }; 
 
 const checkhash = async (req, res) => {
-        const { value, data } = req.body;
+  try {
+        const { value,data } = req.body;
         const kontrolislemi = await bcrypt.compare(this.hash(value), data);
-        res.status(200).json({ kontrolislemi });  
-};
+        res.status(200).json({ kontrolislemi });  }
+      catch (error) {
+        res.status(500).json({ message: 'Hash Doğrulama Hatası!' });
+      }
+}; 
 
   module.exports = {
     hash,
