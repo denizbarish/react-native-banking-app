a423b026f2cda049424aa4cb72ec0dc872a76d35
EmirhanErc32 <your@email.com>
2025-12-16 11:56:44 +0300

    hash

---
+++ backend/src/controllers/hash.controller.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcryptjs');
+const hash = async (req, res) => {
+      const { data } = req.body;
+      const salt = await bcrypt.genSalt(10);
+      const hashdata = await bcrypt.hash(data, salt);
+      res.status(200).json({ hashdata });
+  }; 
+  const checkhash = async (req, res) => {
+        const { data, value } = req.body;
+        const kontrolislemi = await bcrypt.compare(value, data);
+        res.status(200).json({ kontrolislemi });  
+  };
+  module.exports = {
+    hash,
+    checkhash
+  };
