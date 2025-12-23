a02eec42f04079eb6b4ef6f9904487ce50341143
mertokumus01 <okumus01mert@hotmail.com>
2025-12-23 14:28:18 +0300

    kart ekledik

--- backend/src/controllers/card.controller.js
+++ backend/src/controllers/card.controller.js
@@ -1,12 +1,33 @@
 const checklimit = async (req, res) => {
-
+//kart no gelicek , check infoya istek aıcak dgelen veri ile limit kontrolu yapılıp cevap verilecek
 }; 
 
 const checkskt = async (req, res) => {
-
+// kart no gelicek , check infoya istek aıcak dgelen veri ile skt kontrolu yapılıp cevap verilecek
 }; 
+
+const checkinfo = async (req, res) => {
+// kart bilgileri gelicek , doğru ise kart bilgileri döndürülecek.
+};
+
+const   checkdou = async (req, res) => {
+// kart no gelicek , check infoya istek aıcak dgelen veri ile kart no kontrolu yapılıp cevap verilecek
+};
+
+const getharcamalar = async (req, res) => {
+// kart no gelicek , check infoya istek aıcak dgelen veri ile harcamalar döndürülecek
+};
+
+const checkesktre =  async (req, res) => {
+// kart no istek atıcak ekstreyi döndürü  ceklecek
+};
+
 
   module.exports = {
     checklimit,
-    checkskt
+    checkskt,
+    checkinfo,
+    checkdou,
+    getharcamalar,
+    checkesktre
   };
